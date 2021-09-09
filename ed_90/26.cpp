81e1df22c6345da95a8d2f00e0bd99c0c03c75e3
chizuchizu <ochimasumi@gmail.com>
2021-09-09 20:31:44 +0900

    意地でも通すよ二部グラフ 明日は来るよ私にも

--- ed_90/26.cpp
+++ ed_90/26.cpp
@@ -1,16 +1,18 @@
 #define _GLIBCXX_DEBUG
+
 #include <bits/stdc++.h>
+
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
 using namespace std;
 
-vector<int> G[1 << 10];
-int col[1 << 10];
+vector<int> G[100009];
+int col[100009];
 // fill(col, col + 1 << 10, -1)
 
 void dfs(int pos, int cur) {
     col[pos] = cur;
-    for (int i: G[pos]){
+    for (int i: G[pos]) {
         if (col[i] == -1) {
             dfs(i, 1 - cur);
         }
@@ -20,50 +22,36 @@ void dfs(int pos, int cur) {
 int main() {
     int N;
     cin >> N;
-    fill(col, col+N+1, -1);
+    fill(col, col + N + 1, -1);
 
-    rep(i, N-1){
+    rep(i, N - 1) {
         int A, B;
         cin >> A >> B;
         G[A].push_back(B);
         G[B].push_back(A);
     }
 
-    dfs(1, 1);
+    dfs(1, 0);
 
-    int cnt = 0;
     vector<int> G1, G2;
     rep(i, N) {
-        if (col[i] == 0) {
+        if (col[i + 1] == 0) {
             G1.push_back(i);
         } else {
             G2.push_back(i);
         }
     }
 
-    if (G1.size() < G2.size()) {
-        for (int i = 1; i <= N; i++) {
-            if (col[i] == 1) {
-                cout << i << " ";
-                cnt += 1;
-            }
-            if (cnt * 2 >= N) {
-                break;
-            }
+    if (G1.size() > G2.size()) {
+        for (int i = 0; i < N / 2; i++) {
+            cout << G1[i] + 1 << " ";
         }
-        cout << endl;
     } else {
-        for (int i = 1; i <= N; i++) {
-            if (col[i] == 0) {
-                cout << i << " ";
-                cnt += 1;
-            }
-            if (cnt * 2 >= N) {
-                break;
-            }
+        for (int i = 0; i < N / 2; i++) {
+            cout << G2[i] + 1 << " ";
         }
-        cout << endl;
     }
+    cout << endl;
 
     return 0;
 }
